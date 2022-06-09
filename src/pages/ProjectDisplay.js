7bce36e27068cdd7d593fa2b56a11056bef35b8e
Mehedi Hassan <rehan51322@gmail.com>
2022-06-09 16:30:22 +0600

    projects page com

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -11,10 +11,33 @@ function ProjectDisplay() {
     <div className="project">
       <h1> {project.name}</h1>
       <img src={project.image} />
-      <p>
-        <b>Skills:</b> {project.skills}
+      <p className="mt-4 mx-10">
+        <b className="text-2xl">Description:</b>{" "}
+        <span className="text-md">{project.desc}</span>
       </p>
-      <GitHubIcon />
+      <p className="mt-4">
+        <b className="text-2xl">Technology Used:</b>{" "}
+        <span className="text-md">{project.tech}</span>
+      </p>
+      <br />
+      <div>
+        <a
+          href={project.live}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-xl font-semibold text-green-400 font-mono bg-gray-600 rounded p-2 mr-4"
+        >
+          Live Site
+        </a>
+        <a
+          href={project.git}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-xl font-semibold text-violet-400 font-mono bg-gray-600 rounded p-2 mr-4"
+        >
+          Github Code
+        </a>
+      </div>
     </div>
   );
 }
