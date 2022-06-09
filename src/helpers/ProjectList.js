ecf7e220cb915f067b821103b3c25cd21f9a8911
Mehedi Hassan <rehan51322@gmail.com>
2022-06-09 17:49:24 +0600

    projects completed

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,6 +2,9 @@ export const ProjectList = [
   {
     name: "Pc Monkey",
     image: "https://i.ibb.co/n6GLvrG/pc-monkey.png",
+    img1: "https://i.ibb.co/NnXynGY/pc-mon-2.png",
+    img2: "https://i.ibb.co/FWyR0nc/pc-mon-3.png",
+    img3: "https://i.ibb.co/x7jN9jV/pc-mon-4.png",
     tech: "React, Tailwind, JavaScript, HTML, CSS, Firebase, React Router, React Toastify, DaisyUi, Stripe, MongoDB, Axios",
     desc: "This website is pc parts manufacturer who sell parts of pc in wholesale. This website is made with react, firebase, mongodb, express. this is a full responsive website. This website implements full CRUD operation. all buttons are functional here. This site also has a Stripe payment gateway system implemented, users can for their ordered products by sample test cards. This site also has a separate dashboard for user and admin.",
     live: "https://pc-monkey.web.app/",
@@ -10,6 +13,9 @@ export const ProjectList = [
   {
     name: "KahabarChai.com",
     image: "https://i.ibb.co/mt0bcZy/food-app.png",
+    img1: "https://i.ibb.co/SJh5BRL/food1.png",
+    img2: "https://i.ibb.co/KFQnFpS/food2.png",
+    img3: "https://i.ibb.co/X4vjQ4R/food3.png",
     tech: "React, Tailwind, JavaScript, HTML, CSS, Firebase, React Router, React Toastify, MongoDB, Axios",
     desc: "KhabarChai.com is a food inventory MERN-Stack website with CRUD operation.(fully responsive), This website implements full CRUD operation. All buttons are functional here. Integrated 3rd-party components from Tailwind, DaisyUi & for elegent UI/UX. Upon login, a user can Deliver or Restock the food item quantity. Users can also add any food item. Jwt authorization is implemented on this site for which only authorized users can see his/her added items only.",
     live: "https://food-inventory-app.web.app/",
@@ -18,6 +24,9 @@ export const ProjectList = [
   {
     name: "Dream Photography",
     image: "https://i.ibb.co/tJt2g2V/dream-photo.png",
+    img1: "https://i.ibb.co/M7mmNTT/dream1.png",
+    img2: "https://i.ibb.co/X5L85bW/dream2.png",
+    img3: "https://i.ibb.co/sWM4p53/dream3.png",
     tech: "React, Tailwind, JavaScript, HTML, CSS, Firebase, React Router, React Toastify",
     desc: "This is a photography event site. Where users can order for any event photography. Has total of 3 bundle pack.Each bundle has a dynamic route button. Firebase authentication was implemented for login and signup.",
     live: "https://dream-photography-app.web.app/",
@@ -26,6 +35,9 @@ export const ProjectList = [
   {
     name: "Review Loader",
     image: "https://i.ibb.co/TBprkFk/review-loader.png",
+    img1: "https://i.ibb.co/R039dLK/review1.png",
+    img2: "https://i.ibb.co/S7vXTGf/review3.png",
+    img3: "https://i.ibb.co/tPbRn54/review2.png",
     tech: "React, Bootstrap, JavaScript, HTML, CSS, React Router,Recharts",
     desc: "This is SPA product review website. Used Bootstrap for styling. Has a review section for the users(not dynamic). React Charts was implemented in the dashboard route.",
     live: "https://review-loader.netlify.app/",
@@ -34,6 +46,9 @@ export const ProjectList = [
   {
     name: "Loki Convention Center",
     image: "https://i.ibb.co/yWtBPBj/loki-convo.png",
+    img1: "https://i.ibb.co/g6w1P9n/loki1.png",
+    img2: "https://i.ibb.co/tmHnZNb/loki2.png",
+    img3: "https://i.ibb.co/hZ9wm3S/loki3.png",
     tech: "Bootstrap, JavaScript, HTML, CSS",
     desc: "This is a SPA website. Bootstrap was used for designing this website. It has a nice looking carousel.",
     live: "https://mehedis-convention-center.netlify.app/",
@@ -42,6 +57,9 @@ export const ProjectList = [
   {
     name: "Photo Shop",
     image: "https://i.ibb.co/1d0zN7X/influ-pro.png",
+    img1: "https://i.ibb.co/jGW5gfL/pro1.png",
+    img2: "https://i.ibb.co/vwZ0M0b/pro2.png",
+    img3: "https://i.ibb.co/DfRxwk1/pro3.png",
     tech: "Bootstrap, JavaScript, HTML, CSS",
     desc: "This is also a static product website. Designed by Bootstrap and CSS.",
     live: "https://products-for-influencers.netlify.app/",
--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,44 +1,107 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList";
-import GitHubIcon from "@material-ui/icons/GitHub";
 import "../styles/ProjectDisplay.css";
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+
 
 function ProjectDisplay() {
   const { id } = useParams();
   const project = ProjectList[id];
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    focusOnSelect: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="project">
-      <h1> {project.name}</h1>
-      <img src={project.image} />
-      <p className="mt-4 mx-10">
-        <b className="text-2xl">Description:</b>{" "}
-        <span className="text-md">{project.desc}</span>
-      </p>
-      <p className="mt-4">
-        <b className="text-2xl">Technology Used:</b>{" "}
-        <span className="text-md">{project.tech}</span>
-      </p>
-      <br />
-      <div>
-        <a
-          href={project.live}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-xl font-semibold text-green-400 font-mono bg-gray-600 rounded p-2 mr-4"
-        >
-          Live Site
-        </a>
-        <a
-          href={project.git}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-xl font-semibold text-violet-400 font-mono bg-gray-600 rounded p-2 mr-4"
-        >
-          Github Code
-        </a>
+    <>
+      <div className="w-4/5 x mx-auto mt-5 mb-0">
+        {/* <h2> Single Item</h2>  */}
+        <Slider {...settings}>
+          <div>
+            <img src={project.image} />
+          </div>
+          <div>
+            <img src={project.img1} />
+          </div>
+          <div>
+            <img src={project.img2} />
+          </div>
+          <div>
+            <img src={project.img3} />
+          </div>
+        </Slider>
       </div>
-    </div>
+      <div className="project -mt-24 mb-0">
+        <h1> {project.name}</h1>
+        {/* <img src={project.image} />  */}
+        <p className="mx-10">
+          <b className="text-2xl">Description:</b>{" "}
+          <span className="text-md">{project.desc}</span>
+        </p>
+        <p className="mt-4">
+          <b className="text-2xl">Technology Used:</b>{" "}
+          <span className="text-md">{project.tech}</span>
+        </p>
+        <br />
+        <div>
+          <a
+            href={project.live}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xl font-semibold text-green-400 font-mono bg-gray-600 rounded p-2 mr-4"
+          >
+            Live Site
+          </a>
+          <a
+            href={project.git}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xl font-semibold text-violet-400 font-mono bg-gray-600 rounded p-2 mr-4"
+          >
+            Github Code
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
